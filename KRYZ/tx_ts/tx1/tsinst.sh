520dd1913d311b98bd4a27886cdd0a955a24da81
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-04 14:38:13 +0000

    🫦

--- KRYZ/tx_ts/tx1/tsinst.sh
+++ KRYZ/tx_ts/tx1/tsinst.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# This bash srcript is for installing the KL docker image here
+# Bash Script for installing ts packages
 clear
 
 # Colors
@@ -14,45 +14,13 @@ export NC='\033[0m' # No Color
 
 # Commands
 
-hea1() {
+header() {
     echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
     echo -e "${PURPLE}$1${NC}"
     echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
 }
 
-b1() {
-    hea1 "UV Jupyter Lan Installation Commands"
-}
-
-c1() {
-    # Get name of Project
-    echo -e "Enter the name of the project: "
-    read name_of_project
-    if [ -z "$name_of_project" ]; then
-        echo -e "${RED}BASTARD ! Project name cannot be empty${NC}"
-        exit 1
-    fi
-
-    # Commands to execute
-    CO1="uv init $name_of_project"
-    CO2="cd $name_of_project"
-    CO3="uv add --dev ipykernel"
-    CO4="uv tree"
-
-    # Show Commands
-    echo -e ""
-    echo -e "${GREEN}---Commands to execute:---"
-    echo -e ">$CO1"
-    echo -e ">$CO2"
-    echo -e ">$CO3"
-    echo -e ">$CO4"
-    echo -e "Executing....${NC}"
-
-    # Execution Commands
-    eval "$CO1"
-    eval "$CO2"
-    eval "$CO3"
-    eval "$CO4"
+bun_addz() {
 
 }
 
