6271e298dd780c8c0369d06d43c4bd96c85a2143
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-04 18:25:17 +0000

    🫦

--- KRYZ/tx_ts/tx1/src/alc.ts
+++ KRYZ/tx_ts/tx1/src/alc.ts
@@ -7,16 +7,35 @@ const dotenv = require('dotenv')
 dotenv.config()
 const { ALK, WA1K, WA2 } = process.env
 
-const settings = {
-  apiKey: ALK,
-  network: Network.ETH_SEPOLIA,
-}
-const alchemy = new Alchemy(settings)
-const sendval = '0.000000000000000001'
-
 let wallet = new Wallet(WA1K)
 
 async function send_sepolia() {
+  const settings = {
+    apiKey: ALK,
+    network: Network.ETH_SEPOLIA,
+  }
+  const alchemy = new Alchemy(settings)
+  const sendval = '0.000000000000000001'
+
+  const nonce = await alchemy.core.getTransactionCount(wallet.address, 'latest')
+
+  let transaction = {
+    to: WA2,
+    value: Utils.parseUnits(sendval),
+    gasLimit: '21000',
+    maxPriorityFeePerGas: Utils.parseUnits('5', 'gwei'),
+    maxFeePerGas: Utils.parseUnits('20', 'gwei'),
+    nonce: nonce,
+    type: 2,
+    chainId: 11155111,
+  }
+
+  let rawTransaction = await wallet.signTransaction(transaction)
+  let tx = await alchemy.core.sendTransaction(rawTransaction)
+  console.log('Sent transaction', tx)
+}
+
+async function send_holeksy() {
   const nonce = await alchemy.core.getTransactionCount(wallet.address, 'latest')
 
   let transaction = {
