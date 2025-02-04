150aec12eed0e7f5011763ba40b6524674946062
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-04 15:01:03 +0000

    🫦

--- KRYZ/tx_ts/tx1/src/uti.ts
+++ KRYZ/tx_ts/tx1/src/uti.ts
@@ -1,15 +1,17 @@
 // File is for writing functiont that make colorful headers
 
-import chalk from 'chalk';
+import chalk from 'chalk'
 
 export function header(text: string): void {
-    const border = '=========';
-    const styledBorder = chalk.bold.greenBright(border);
-    const styledText = chalk.bold.greenBright(text);
-  
-    console.log(`
-  ${styledBorder}
-  ${styledText}
-  ${styledBorder}
-    `.trim());
-  }
+  const border = '========='
+  const styledBorder = chalk.bold.greenBright(border)
+  const styledText = chalk.bold.greenBright(text)
+
+  console.log(
+    `
+${styledBorder}
+${styledText}
+${styledBorder}
+`.trim()
+  )
+}
