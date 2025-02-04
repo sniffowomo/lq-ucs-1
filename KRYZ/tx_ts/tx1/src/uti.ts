a8477c392d3cae7927745e251677189bd4fcd550
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-04 15:18:41 +0000

    🫦

--- KRYZ/tx_ts/tx1/src/uti.ts
+++ KRYZ/tx_ts/tx1/src/uti.ts
@@ -1,7 +1,7 @@
 // File is for writing functiont that make colorful headers
 
-import chalkAnimation from 'chalk-animation'
 import chalk from 'chalk'
+import ora from 'ora'
 
 export function header(text: string): void {
   const border = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
