0738b83b4691cb34bc1a328f65e29dd15f1fad6d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-04 15:28:42 +0000

    🫦

--- KRYZ/tx_ts/tx1/index.ts
+++ KRYZ/tx_ts/tx1/index.ts
@@ -1,10 +1,9 @@
 // Main entry point
 
-import { header, showSpinnerWithStates } from './src/uti'
+import { header } from './src/uti'
 
 function main() {
   console.clear()
-  showSpinnerWithStates()
   header('Hello World! Smell her fars')
 }
 
