396a3485eca21e49a329fd534de2e51a013f0c3d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-04 18:27:25 +0000

    🫦

--- KRYZ/tx_ts/tx1/src/alc.ts
+++ KRYZ/tx_ts/tx1/src/alc.ts
@@ -37,3 +37,29 @@ async function send_sepolia() {
   let tx = await alchemy.core.sendTransaction(rawTransaction)
   console.log('Sent transaction', tx)
 }
+
+async function send_sepolia() {
+  const settings = {
+    apiKey: ALK,
+    network: Network.ETH_SEPOLIA,
+  }
+  const alchemy = new Alchemy(settings)
+  const sendval = '0.000000000000000001'
+
+  const nonce = await alchemy.core.getTransactionCount(wallet.address, 'latest')
+
+  let transaction = {
+    to: WA2,
+    value: Utils.parseUnits(sendval),
+    gasLimit: '21000',
+    maxPriorityFeePerGas: Utils.parseUnits('5', 'gwei'),
+    maxFeePerGas: Utils.parseUnits('20', 'gwei'),
+    nonce: nonce,
+    type: 2,
+    chainId: 11155111,
+  }
+
+  let rawTransaction = await wallet.signTransaction(transaction)
+  let tx = await alchemy.core.sendTransaction(rawTransaction)
+  console.log('Sent transaction', tx)
+}
