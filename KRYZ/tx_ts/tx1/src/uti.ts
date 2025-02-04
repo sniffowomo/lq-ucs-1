9af19a8d4e6d8a28b7c811cb4d4d0e717dd374de
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-04 15:41:20 +0000

    🫦

--- KRYZ/tx_ts/tx1/src/uti.ts
+++ KRYZ/tx_ts/tx1/src/uti.ts
@@ -16,31 +16,3 @@ ${styledBorder}
 `.trim()
   )
 }
-
-export function showSpinner(text: string): ora.Ora {
-  return ora(text).start()
-}
-
-export function updateSpinner(spinner: ora.Ora, text: string): void {
-  spinner.text = text
-}
-
-export function succeedSpinner(spinner: ora.Ora, text: string): void {
-  spinner.succeed(text)
-}
-
-export function failSpinner(spinner: ora.Ora, text: string): void {
-  spinner.fail(text)
-}
-
-export function warnSpinner(spinner: ora.Ora, text: string): void {
-  spinner.warn(text)
-}
-
-export function infoSpinner(spinner: ora.Ora, text: string): void {
-  spinner.info(text)
-}
-
-export function stopSpinner(spinner: ora.Ora): void {
-  spinner.stop()
-}
