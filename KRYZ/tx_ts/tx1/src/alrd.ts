8bdf0566094ac2fde1a105a43b3013ec928e4c8d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-05 05:11:35 +0000

    🫦

--- KRYZ/tx_ts/tx1/src/alrd.ts
+++ KRYZ/tx_ts/tx1/src/alrd.ts
@@ -16,8 +16,5 @@ export async function getTxRcpt() {
   const txHash =
     '0x4cc90797974e32222b5f393028ea6b412e1798336d0e05831c1f03fb18c0f9c4'
 
-  alchemy.core
-    .getTransactionReceipt(txHash)
-    // Transaction hash of the transaction for which you want to get information.)
-    .then(console.log)
+  alchemy.core.getTransactionReceipt(txHash).then(console.log)
 }
