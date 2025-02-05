af3e038c643dc9bfbe1e22723c41d6741232aaec
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-05 05:04:04 +0000

    🫦

--- KRYZ/tx_ts/tx1/src/alrd.ts
+++ KRYZ/tx_ts/tx1/src/alrd.ts
@@ -4,4 +4,16 @@ import { Alchemy, Network, Wallet, Utils } from 'alchemy-sdk'
 import dotenv from 'dotenv'
 import chalk from 'chalk'
 
-export async function send_sepolia() {}
+export async function getTxRcpt() {
+  const settings = {
+    apiKey: '<-- ALCHEMY API KEY -->', // Replace with your Alchemy API Key.
+    network: Network.ETH_MAINNET, // Replace with your network.
+  }
+  const alchemy = new Alchemy(settings)
+
+  alchemy.core
+    .getTransactionReceipt(
+      '0x68ea69fd8b5dfa589a7a983c324ab153a33356320207885a9bba84425598dcaa' // Transaction hash of the transaction for which you want to get information.
+    )
+    .then(console.log)
+}
