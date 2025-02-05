a22e4b8ffb540a7ba4ec9ba50acc9e9a7aaeef8d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-05 05:24:01 +0000

    🫦

--- KRYZ/tx_ts/tx1/src/alrd.ts
+++ KRYZ/tx_ts/tx1/src/alrd.ts
@@ -36,18 +36,17 @@ export async function getTxHistory() {
   const fromAddress = 'vitalik.eth'
 
   // Main function to fetch all transactions for an address
-  const main = async () => {
-    try {
-      // Get all transactions for an address from block 0 and store in txns
-      const txns = await alchemy.core.getAssetTransfers({
-        fromBlock: '0x0',
-        fromAddress: fromAddress,
-        category: ['external', 'internal', 'erc20', 'erc721', 'erc1155'],
-      })
 
-      console.log(txns)
-    } catch (error) {
-      console.error('Error fetching transaction history:', error)
-    }
+  try {
+    // Get all transactions for an address from block 0 and store in txns
+    const txns = await alchemy.core.getAssetTransfers({
+      fromBlock: '0x0',
+      fromAddress: fromAddress,
+      category: ['external', 'internal', 'erc20', 'erc721', 'erc1155'],
+    })
+
+    console.log(txns)
+  } catch (error) {
+    console.error('Error fetching transaction history:', error)
   }
 }
