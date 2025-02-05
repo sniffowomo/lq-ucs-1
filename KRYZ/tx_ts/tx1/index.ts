9152207f0f5499d606f3b2b45696c2b321079272
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-02-05 04:22:35 +0000

    🫦

--- KRYZ/tx_ts/tx1/index.ts
+++ KRYZ/tx_ts/tx1/index.ts
@@ -1,6 +1,11 @@
 import ora from 'ora'
 import { header } from './src/uti'
-import { send_sepolia, send_holeksy } from './src/alcsnd'
+import {
+  send_sepolia,
+  send_holeksy,
+  sendSepoliaPrivate,
+  sendHoleksyPrivate,
+} from './src/alcsnd'
 
 async function sendingtx() {
   // Spinner for sending to Sepolia
@@ -19,6 +24,30 @@ async function sendingtx() {
   try {
     header('Send Holeksy')
     await send_holeksy()
+    holeksySpinner.succeed('Successfully sent to Holeksy')
+  } catch (error) {
+    holeksySpinner.fail('Failed to send to Holeksy')
+    console.error('Error sending to Holeksy:', error)
+  }
+}
+
+async function sendingPrivateTxs() {
+  // Spinner for sending to Sepolia
+  const sepoliaSpinner = ora('Sending to Sepolia').start()
+  try {
+    header('Send Sepolia Private')
+    await sendSepoliaPrivate()
+    sepoliaSpinner.succeed('Successfully sent to Sepolia')
+  } catch (error) {
+    sepoliaSpinner.fail('Failed to send to Sepolia')
+    console.error('Error sending to Sepolia:', error)
+  }
+
+  // Spinner for sending to Holeksy
+  const holeksySpinner = ora('Sending to Holeksy').start()
+  try {
+    header('Send Holeksy Private')
+    await sendHoleksyPrivate()
     holeksySpinner.succeed('Successfully sent to Holeksy')
   } catch (error) {
     holeksySpinner.fail('Failed to send to Holeksy')
